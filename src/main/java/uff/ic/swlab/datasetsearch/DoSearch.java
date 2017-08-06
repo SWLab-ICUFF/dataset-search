19cd307e0899f4bc332c0c149cce0d859521ef8f
Luiz Leme <lapaesleme@gmail.com>
2017-08-06 18:19:34 -0300

    ok

--- src/main/java/uff/ic/swlab/datasetsearch/DoSearch.java
+++ src/main/java/uff/ic/swlab/datasetsearch/DoSearch.java
@@ -1,7 +1,10 @@
 package uff.ic.swlab.datasetsearch;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.URL;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,22 +15,44 @@ import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
 
 public class DoSearch extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
         try {
-
-            String query = request.getParameter("q");
+            String path = request.getPathInfo();
+            Lang lang = detectRequestedLang(request.getHeader("Accept"));
+            String query = URLDecoder.decode(request.getParameter("q"), "UTF-8");
+            System.out.println(query);
             int offset = Integer.parseInt(request.getParameter("offset"));
             int limit = Integer.parseInt(request.getParameter("limit"));
 
-            if ((new UrlValidator()).isValid(query))
-                doVoidBasedSearch(response, new URL(query));
-            else
-                doKeywordSearch(response, query);
+            if (lang == null) {
+                String resource = ("" + request.getRequestURL()).replaceFirst("http://", "http/")
+                        + "?q=" + URLEncoder.encode(query, "UTF-8") + "&offset=" + offset + "&limit=" + limit;
 
+                String url = "http://linkeddata.uriburner.com/about/html/" + resource + "&@Lookup@=&refresh=clean";
+                response.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
+                response.setHeader("Location", url);
+            } else
+                try (OutputStream httpReponse = response.getOutputStream()) {
+                    Model model;
+                    if ((new UrlValidator()).isValid(query))
+
+                        model = doVoidBasedSearch(response, new URL(query));
+                    else
+                        model = doKeywordSearch(response, query);
+
+                    if (model.size() > 0) {
+                        response.setContentType(lang.getContentType().getContentType());
+                        RDFDataMgr.write(httpReponse, model, lang);
+                        httpReponse.flush();
+                    } else
+                        response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+
+                }
         } catch (Exception e) {
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             response.setHeader("Location", "http://localhost:8080/dataset-search/");
@@ -40,39 +65,36 @@ public class DoSearch extends HttpServlet {
         response.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
     }
 
-    private void doVoidBasedSearch(HttpServletResponse response, URL datasetURI) {
-        response.setStatus(HttpServletResponse.SC_NOT_IMPLEMENTED);
+    private Model doVoidBasedSearch(HttpServletResponse response, URL datasetURI) {
+        Model model = ModelFactory.createDefaultModel();
+        return model;
     }
 
-    private void doKeywordSearch(HttpServletResponse response, String keywords) {
+    private Model doKeywordSearch(HttpServletResponse response, String keywords) {
+
         String queryString = "prefix dcterms: <http://purl.org/dc/terms/>\n"
                 + "prefix void: <http://rdfs.org/ns/void#>\n"
                 + "prefix text: <http://jena.apache.org/text#>\n"
                 + "prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
                 + "prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"
                 + "\n"
-                + "construct {?s a void:Dataset.\n"
-                + "           ?s rdfs:label ?title.\n"
-                + "           ?s dcterms:description ?description.\n"
-                + "           ?s rdf:seeAlso ?seeAlso.}\n"
+                + "construct {?s rdfs:label ?title.}\n"
                 + "where {\n"
-                + "  graph ?g {?s text:query (dcterms:description 'brazil') ;\n"
-                + "               dcterms:title ?title;\n"
-                + "               dcterms:description ?description;\n"
-                + "               rdfs:seeAlso ?seeAlso.}\n"
+                + "  graph ?g {?s text:query (dcterms:description '%s') ;\n"
+                + "               dcterms:title ?title.}\n"
                 + "}";
+        queryString = String.format(queryString, keywords);
 
         Model model = ModelFactory.createDefaultModel();
         QueryExecution q = QueryExecutionFactory.sparqlService("http://localhost:8080/fuseki/DatasetDescriptions/sparql", queryString);
         q.execConstruct(model);
 
-        response.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
-        response.setHeader("Location", "http://localhost:8080/dataset-search/");
+        return model;
     }
 
     private Lang detectRequestedLang(String accept) {
-        Lang[] langs = {Lang.RDFXML, Lang.TURTLE, Lang.TTL, Lang.TRIG, Lang.TRIX, Lang.JSONLD, Lang.RDFJSON,
-            Lang.RDFTHRIFT, Lang.NQUADS, Lang.NQ, Lang.NTRIPLES, Lang.N3, Lang.NT};
+        Lang[] langs = {Lang.RDFXML, Lang.TURTLE, Lang.TTL, Lang.NTRIPLES, Lang.N3, Lang.NT,
+            Lang.TRIG, Lang.TRIX, Lang.JSONLD, Lang.RDFJSON, Lang.RDFTHRIFT, Lang.NQUADS, Lang.NQ};
         for (Lang lang : langs)
             if (accept.toLowerCase().contains(lang.getHeaderString().toLowerCase()))
                 return lang;
